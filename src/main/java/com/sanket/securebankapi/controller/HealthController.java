ac468c76dc1171663f240e9e6bf4e88855e41533
Sanket Oza <sanketoza.rock@gmail.com>
2026-06-06 20:30:09 +0530

    Initial Spring Boot application with health endpoint

---
+++ src/main/java/com/sanket/securebankapi/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.sanket.securebankapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Secure Bank API is running";
+    }
+}
