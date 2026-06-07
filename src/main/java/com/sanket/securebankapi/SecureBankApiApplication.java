0d27df8b384deb4847018771d1d979524e24f845
Sanket Oza <sanketoza.rock@gmail.com>
2026-06-07 21:51:14 +0530

    Testing Java secret

--- src/main/java/com/sanket/securebankapi/SecureBankApiApplication.java
+++ src/main/java/com/sanket/securebankapi/SecureBankApiApplication.java
@@ -9,6 +9,8 @@ public class SecureBankApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SecureBankApiApplication.class, args);
 
+		String secret = "AWS_SECRET_ACCESS_KEY=ABCD123456789SECRET";
+
 	}
 
 }
